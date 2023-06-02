956aac260e5bfa5a59d9649896e3b3dc5e6b6347
Diogo <diogo007@gmail.com>
2023-06-02 17:19:48 -0300

    new memory page

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,7 +1,8 @@
 // styling
 import { styled } from 'nativewind';
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import { ImageBackground } from 'react-native';
 
 // relative imports
 import blurBG from '../src/assets/bg-blur.png';
@@ -12,15 +13,34 @@ import { useFonts } from 'expo-font';
 import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
 import { SplashScreen, Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
 
 // now it is possible to use tialwind styling on the svg
 const StyledStripes = styled(Stripes);
 export default function Layout() {
+  // useState retutns an array with a variable and the function that
+  // must be used to alter that variable so that the changes are propagated
+  // it takes a generic with the possible types and a starting value
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null);
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   });
+
+  // hook without watching any variable, it executes only once on the
+  // component loading
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) =>
+      // little hack to turn the string into a boolean
+      {
+        setIsUserAuthenticated(!!token);
+      }
+    );
+  }, []);
 
   if (!hasLoadedFonts) {
     return <SplashScreen />;
@@ -39,7 +59,12 @@ export default function Layout() {
           headerShown: false,
           contentStyle: { backgroundColor: 'transparent' },
         }}
-      />
+      >
+        {/* redirect automatically from index to memories in case the user is authenticated */}
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="new" />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   );
 }
