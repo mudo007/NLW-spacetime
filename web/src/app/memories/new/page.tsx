eba3c68d2a5415d69a36d733c7af0298af5a6742
Diogo <diogo007@gmail.com>
2023-06-04 15:56:44 -0300

    media preview web

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,3 +1,4 @@
+import { MediaPicker } from '@/components/MediaPicker'
 import { Camera, ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 
@@ -34,8 +35,7 @@ export default function NewMemory() {
             Tornar memória pública
           </label>
         </div>
-        {/* invisible input elements tag can be anywhere without affecting the layout */}
-        <input type="file" id="media" className="invisible h-0 w-0" />
+        <MediaPicker />
         <textarea
           name="content"
           spellCheck={false}
